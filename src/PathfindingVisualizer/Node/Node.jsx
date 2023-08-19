27d0a45126f2e56adeba0342fb1362a15a052ee1
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-19 22:06:52 +1000

    Add start and end grid, green for start and red for end

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,5 +1,6 @@
 import './Node.css';
 
-export default function Node() {
-  return <div className='node'></div>
+export default function Node(props) {
+  const {position} = props;
+  return <div className={`node ${position}`}></div>
 }
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -3,17 +3,24 @@ import Node from './Node/Node';
 import './PathfindingVisualizer.css'
  
 export default function PathfindingVisualizer() {
-  const rows = 30;
+  const rows = 50;
   const cols = 20;
 
 
   return (
+    // Creates a grid
     <div className='grid'>
       {[...Array(rows)].map((_, rowIndex) => (
         <div className='row' key={rowIndex}>
-        {[...Array(cols)].map((_, colIndex) => (
-          <Node key={`${rowIndex}-${colIndex}`} />
-        ))}
+        {[...Array(cols)].map((_, colIndex) => {
+          let position = '';
+          if (rowIndex === 3 && colIndex === 6) {
+            position = 'start';
+          } else if (rowIndex === 28 && colIndex === 6) {
+            position = 'end';
+          }
+          return <Node key={`${rowIndex}-${colIndex}`} position={position}/>
+        })}
       </div>
       ))}
     </div>
