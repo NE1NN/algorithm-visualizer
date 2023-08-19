c3d41a69aa18d1fa12d3052440a87fe066efb7f0
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-20 00:58:55 +1000

    Remove visited tile from start and end node

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -2,7 +2,15 @@ import './Node.css';
 
 export default function Node(props) {
   const {isStart, isEnd, isVisited} = props;
-  const className = `node ${isStart ? 'start' : ''} ${isEnd ? 'end' : ''} ${isVisited ? 'visited' : ''}`;
+  // const className = `node ${isStart ? 'start' : ''} ${isEnd ? 'end' : ''} ${isVisited ? 'visited' : ''}`;
+  let className = "node";
+  if (isStart) {
+    className = "node start";
+  } else if (isEnd) {
+    className = "node end";
+  } else if (isVisited) {
+    className = "node visited";
+  }
 
   return <div className={className}></div>;
 }
