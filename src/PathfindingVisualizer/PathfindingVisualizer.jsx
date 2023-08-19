d88ef3b499f23427cdab3914339217e632ae9a51
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-19 23:47:09 +1000

    Use useState to make an initial grid

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -7,11 +7,28 @@ export default function PathfindingVisualizer() {
   const rows = 50;
   const cols = 20;
 
+  const startNode = [3, 6];
+  const endNode = [28, 6];
+
   // 2D array to keep track of visited nodes
   const [visitedNodes, setVisitedNodes] = useState(Array(rows).fill().map(() => Array(cols).fill(false)));
 
   // State to track the current node being visited
   const [currentNode, setCurrentNode] = useState(null);
+
+  const [grid, setGrid] = useState(() => {
+    const initialGrid = [];
+    for (let row = 0; row < rows; row++) {
+      const currentRow = []
+      for (let col = 0; col < cols; col++) {
+        const isStart = row === startNode[0] && col === startNode[1];
+        const isEnd = row === endNode[0] && col === endNode[1];
+        currentRow.push({row, col, isStart, isEnd, isVisited: false});
+      }
+      initialGrid.push(currentRow);
+    }
+    return initialGrid
+  })
 
 
   return (
@@ -20,8 +37,8 @@ export default function PathfindingVisualizer() {
       {[...Array(rows)].map((_, rowIndex) => (
         <div className='row' key={rowIndex}>
         {[...Array(cols)].map((_, colIndex) => {
-          const isStart = rowIndex === 3 && colIndex === 6
-          const isEnd = rowIndex === 28 && colIndex === 6
+          const isStart = rowIndex === startNode[0] && colIndex === startNode[1];
+          const isEnd = rowIndex === endNode[0]  && colIndex === endNode[1];
           return <Node key={`${rowIndex}-${colIndex}`} isStart={isStart} isEnd={isEnd}/>
         })}
       </div>
