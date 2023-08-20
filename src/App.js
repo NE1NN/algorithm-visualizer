64f83266d3ca12da1455d0e529d7d134838b58ea
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-20 13:43:11 +1000

    Add navbar class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
