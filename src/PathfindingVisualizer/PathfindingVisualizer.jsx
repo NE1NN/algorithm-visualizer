662a6a05fde057e493782f6ccc1f7532d13773ff
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-20 11:43:46 +1000

    Add an isWall object

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -10,6 +10,7 @@ export const cols = 20;
 export default function PathfindingVisualizer() {
   const startNode = [3, 16];
   const endNode = [34, 2];
+  const wallNode = [[33, 2], [33, 1], [33, 3], [34, 3]];
 
   // Creates initial grid
   const [grid, setGrid] = useState(() => {
@@ -19,7 +20,8 @@ export default function PathfindingVisualizer() {
       for (let col = 0; col < cols; col++) {
         const isStart = row === startNode[0] && col === startNode[1];
         const isEnd = row === endNode[0] && col === endNode[1];
-        currentRow.push({row, col, isStart, isEnd, isVisited: false, isPath: false});
+        const isWall = wallNode.some(([wallRow, wallCol]) => wallRow === row && wallCol === col);
+        currentRow.push({row, col, isStart, isEnd, isVisited: false, isPath: false, isWall});
       }
       initialGrid.push(currentRow);
     }
@@ -36,7 +38,7 @@ export default function PathfindingVisualizer() {
       {grid.map((row, rowIndex) => (
         <div className='row' key={rowIndex}>
         {row.map(node => (
-          <Node key={`${node.row}-${node.col}`} isStart={node.isStart} isEnd={node.isEnd} isVisited={node.isVisited} isPath={node.isPath}/>
+          <Node key={`${node.row}-${node.col}`} isStart={node.isStart} isEnd={node.isEnd} isVisited={node.isVisited} isPath={node.isPath} isWall={node.isWall}/>
         ))}
       </div>
       ))}
