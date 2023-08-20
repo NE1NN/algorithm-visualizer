90c8a67f7cc78305b17581e7d4620d4f0082efdd
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-21 08:43:28 +1000

    Add dropdown to change algorithm and speed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import './Navbar.css';
 import { bfs } from '../Algorithms/bfs';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 export default function Navbar(props) {
   const { grid, startNode, endNode, setGrid } = props;
@@ -8,14 +10,29 @@ export default function Navbar(props) {
     bfs(grid, startNode, endNode, setGrid, 10);
   }
 
+  const algorithmOptions = ['BFS', 'DFS'];
+  const defaultAlgoOption = algorithmOptions[0];
+
+  const speedOptions = ['Fast', 'Medium', 'Slow'];
+  const defaultSpeedOption = speedOptions[0];
+
   return (
     <nav className="navbar">
       <div id="title">Path Finding Algorithm</div>
-      <div>BFS</div>
-      <div>Speed: Fast</div>
+      <Dropdown
+        options={algorithmOptions}
+        value={defaultAlgoOption}
+        placeholder="Select an option"
+      />
+      <Dropdown
+        options={speedOptions}
+        value={defaultSpeedOption}
+        placeholder="Select an option"
+      />
       <button className="startButton" onClick={handleButtonClick}>
         Start
       </button>
+      ;
     </nav>
   );
 }
