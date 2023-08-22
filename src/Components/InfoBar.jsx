7431bca2dbad53b0c3450ff2fe58db954e10223f
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-08-22 23:23:03 +1000

    Add node info to info bar

--- src/Components/InfoBar.jsx
+++ src/Components/InfoBar.jsx
@@ -2,11 +2,23 @@ import './InfoBar.css';
 
 export default function InfoBar() {
   return (
-    <div className="infoBar">
-      <div className="node"></div>
-      <div>Start Node</div>
-      <div className="node"></div>
-      <div>End Node</div>
+    <div className="info-bar">
+      <container>
+        <div className="info-node" id="info-start"></div>
+        <div>Start</div>
+      </container>
+      <container>
+        <div className="info-node" id="info-end"></div>
+        <div>End</div>
+      </container>
+      <container>
+        <div className="info-node" id="info-visited"></div>
+        <div>Visited</div>
+      </container>
+      <container>
+        <div className="info-node" id="info-path"></div>
+        <div>Path</div>
+      </container>
     </div>
   );
 }
